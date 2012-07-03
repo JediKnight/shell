e9aa4c9c6bfda80c831d2fe56d032fe23c478ac3
tomoaki tanaka <tomoaki@bamboo-copter.com>
2012-07-03 18:58:28 +0900

    ログフォーマット修正

--- gitpull.sh
+++ gitpull.sh
@@ -6,5 +6,5 @@ prj='1timeTransfer lib pwmgr social shellscript'
 for p in $prj
 do
     cd ${repodir}/${p}
-    git pull origin master || echo "$p pull error" >> ~/gpull.log
+    git pull origin master || echo "$p pull error [`date +%Y/%m/%d %H:%M:%S`]" >> ~/gpull.log
 done
