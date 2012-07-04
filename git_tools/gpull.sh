20edcf379c1599d24c9b92b6e57023bf83c09f39
il_tanaka <tanaka@infiniteloop.co.jp>
2012-07-04 09:41:02 +0900

    ディレクトリが存在しなかったらcloneする

--- git_tools/gpull.sh
+++ git_tools/gpull.sh
@@ -5,6 +5,7 @@ prj='1timeTransfer lib pwmgr social shellscript'
 
 for p in $prj
 do
+    [ ! -d ${repodir}/${p} ] && git clone http://github.com/jedi-master/${p}.git ${repodir}/${p}
     cd ${repodir}/${p}
     git pull origin master || echo "$p pull error [`date +%Y/%m/%d %H:%M:%S`]" >> ~/gpull.log
 done
